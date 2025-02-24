80c14e115bac0de939fa9bc4a4c6501fe8d97680
Thibault Lemery <thibaultlemery@mbp-de-thibault.home>
2025-02-24 14:32:44 +0100

    edit config for minuet

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -49,8 +49,11 @@ return {
             end_point = 'http://localhost:11434/v1/completions',
             model = 'qwen2.5-coder:7b',
             optional = {
-              max_tokens = 56,
+              max_tokens = 256,
               top_p = 0.9,
+              generationConfig = {
+                maxOutputTokens = 512,
+              },
             },
           },
         },
