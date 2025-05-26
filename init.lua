8ac4f6dc4bffbdd7df389d43b4e2a9715d93de8c
ThbltLmr <lemery.thibault.pro@gmail.com>
2025-05-26 11:53:30 +0200

    disable minuet

--- init.lua
+++ init.lua
@@ -985,3 +985,4 @@ vim.cmd [[cab cc CodeCompanion]]
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+vim.keymap.set('i', '<C-Esc>', '<Plug>(copilot-dismiss)')
