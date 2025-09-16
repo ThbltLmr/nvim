5db3593b0938bbfd7b985fac622d2761faf31424
Thibault Lemery <thibault.lemery@theodo.com>
2025-09-16 18:01:48 +0200

    switch php lsp

--- init.lua
+++ init.lua
@@ -693,7 +693,7 @@ require('lazy').setup({
         -- But for many setups, the LSP (`ts_ls`) will work just fine
         ts_ls = {},
         cssls = {},
-        intelephense = {},
+        phpactor = {},
         --
 
         lua_ls = {
