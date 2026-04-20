e0e8aae80fc188f85f014479d0a3380439613e73
Thibault Lemery <thibault.lemery@theodo.com>
2026-04-20 11:02:48 +0200

    feat: add <leader>MP keybinding for MarkdownPreview

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -51,6 +51,9 @@ return {
     'iamcco/markdown-preview.nvim',
     cmd = { 'MarkdownPreviewToggle', 'MarkdownPreview', 'MarkdownPreviewStop' },
     ft = { 'markdown' },
+    keys = {
+      { '<leader>mp', '<cmd>MarkdownPreview<cr>', desc = 'Markdown Preview' },
+    },
     build = function()
       vim.fn['mkdp#util#install']()
     end,
