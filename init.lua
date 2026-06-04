1fa3131777f16f3657e2b062e4cf599f2d33b3f2
Thibault Lemery <thibault.lemery@theodo.com>
2026-06-04 09:33:07 +0200

    fix(ui): enable nerd font icons (have_nerd_font = true)
    Ghostty renders Nerd Font glyphs via JetBrainsMono Nerd Font Mono, so
    turn on icons. Also installs nvim-web-devicons, fixing the octo review
    file-panel crash (module 'nvim-web-devicons' not found).

--- init.lua
+++ init.lua
@@ -99,7 +99,8 @@ do
   vim.g.maplocalleader = ' '
 
   -- Set to true if you have a Nerd Font installed and selected in the terminal
-  vim.g.have_nerd_font = false
+  -- Ghostty provides built-in Nerd Font symbol fallback, so glyphs render fine.
+  vim.g.have_nerd_font = true
 
   -- [[ Setting options ]]
   --  See `:help vim.o`
